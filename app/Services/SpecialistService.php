7e8e5392d244c97a6a84212566738ed7dfd9320f
Cloudy888 <ahmaddepok01.ak@gmail.com>
2026-09-05 02:29:30 +0700

    Sprint #2 Coding Specialist service
    mempelajari arsitektur MVC, Service & Repository Pattern, Eloquent ORM, relasi database, CRUD, serta pengelolaan file.

--- app/Services/SpecialistService.php
+++ app/Services/SpecialistService.php
@@ -2,7 +2,76 @@
 
 namespace App\Services;
 
+use App\Repositories\SpecialistRepository;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
+
 class SpecialistService
 {
-    // Define your methods and properties for the SpecialistService here
+    private SpecialistRepository $specialistRepository;
+
+    public function __construct(SpecialistRepository $specialistRepository)
+    {
+        $this->specialistRepository = $specialistRepository;
+    }
+
+    public function getAll(array $fields)
+    {
+        return $this->specialistRepository->getall($fields);
+    }
+
+    public function getById(int $id, array $fields)
+    {
+        return $this->specialistRepository->getById($id, $fields);
+    }
+
+    public function create(array $data)
+    {
+        if (isset($data['photo']) && $data['photo'] instanceof UploadedFile) {
+            $data['photo'] = $this->uploadPhoto($data['photo']);
+        }
+
+        return $this->specialistRepository->create($data);
+    }
+
+    public function update(int $id, array $data)
+    {
+        $fields = ['*'];
+        $specialist = $this->specialistRepository->getById($id, $fields);
+
+        if (isset($data['photo']) && $data['photo'] instanceof UploadedFile) {
+            if (!empty($specialist->photo)) {
+                $this->deletePhoto($specialist->photo);
+            }
+            $data['photo'] = $this->uploadPhoto($data['photo']);
+         }
+    
+        return $this->specialistRepository->update($id, $data);
+    }
+
+    public function delete(int $id)
+    {
+        $fields = ['*'];
+        $specialist = $this->specialistRepository->getById($id, $fields);
+
+        if (!empty($specialist->photo)) {
+            $this->deletePhoto($specialist->photo);
+        }
+
+        return $this->specialistRepository->delete($id);
+    }
+
+    private function uploadPhoto(uploadedFile $photo): string
+    {
+       return $photo->store('specialists', 'public');
+       //return true;
+    }
+
+    private function deletePhoto(string $photoPath)
+    {
+        $relativePath = 'specialists/' . basename($photoPath);
+        if (Storage::disk('public')->exists($relativePath)) {
+            Storage::disk('public')->delete($relativePath);
+        }
+    }
 }
