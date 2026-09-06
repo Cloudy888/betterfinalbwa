d69d192ea9453cedd0202c328908196178cf23e4
Cloudy888 <ahmaddepok01.ak@gmail.com>
2026-09-07 01:16:01 +0700

    Sprint #2 Coding transaction repository
    Membuat BookingTransactionRepository beserta method CRUD dan query transaksi

--- app/Repositories/BookingTransactionRepository.php
+++ app/Repositories/BookingTransactionRepository.php
@@ -1,7 +1,62 @@
 <?php
+
 namespace App\Repositories;
+
+use App\Models\BookingTransaction;
 
 class BookingTransactionRepository
 {
-    // Define your methods and properties for the SpecialistRepository here
+
+    //manager queries
+    public function getAll()
+    {
+        return BookingTransaction::with(['doctor', 'doctor.hospital', 'doctor.specialists','user'])
+        ->latest()
+        ->paginate(10);
+    }
+
+    public function getByIdForManager(int $id)
+    {
+        return BookingTransaction::with(['doctor', 'doctor.hospital', 'doctor.specialists', 'user'])
+            ->findOrFail($id);
+    }
+
+    public function updateStatus(int $id, string $status)
+    {
+        $transaction = $this->getByIdForManager($id);
+        $transaction->update(['status'=> $status]);
+        return $transaction;
+    }
+
+
+
+    //costumer queries
+    public function getAllforuser(int $userId)
+    {
+        return BookingTransaction::where('user_id', $userId)
+        ->with(['doctor', 'doctor.hospital', 'doctor.specialists'])
+        ->latest()
+        ->paginate(10);
+    }
+
+    public function getById(int $id, int $userId)
+    {
+        return BookingTransaction::where('id', $id)
+            ->where('user_id', $userId)
+            ->with(['doctor', 'doctor.hospital', 'doctor.specialists'])
+            ->firstOrFail();
+    }
+
+    public function create(array $data)
+    {
+        return BookingTransaction::create($data);
+    }
+
+    public function isTimeSlotTakenForDoctor(int $doctorId, string $date, string $time): bool
+    {
+        return BookingTransaction::where('doctor_id', $doctorId)
+            ->where('started_at', $date)
+            ->where('time', $time)
+            ->exists();
+    }
 }
