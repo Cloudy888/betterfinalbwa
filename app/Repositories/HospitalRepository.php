cd459cd6f68982be1f97a04da4340d73a7d4ce8c
Cloudy888 <ahmaddepok01.ak@gmail.com>
2026-09-06 23:56:50 +0700

    Sprint #2 Hospital repository dan service
    menerapkan dependency injection pada Hospital repository dan service lalu attach and detach

--- app/Repositories/HospitalRepository.php
+++ app/Repositories/HospitalRepository.php
@@ -1,7 +1,35 @@
 <?php
 namespace App\Repositories;
 
+use App\Models\Hospital;
+
 class HospitalRepository
 {
-    // Define your methods and properties for the SpecialistRepository here
+    public function getAll(array $fields)
+    {
+        return Hospital::select($fields)->latest()->paginate(10);
+    }
+
+    public function getById(int $id, array $fields)
+    {
+        return Hospital::select($fields)->with(['doctors.specialists','specialists'])->findOrFail($id);
+    }
+
+    public function create(array $data)
+    {
+        return Hospital::create($data);
+    }
+
+    public function update(int $id, array $data)
+    {
+        $hospital = Hospital::findOrFail($id);
+        $hospital->update($data);
+        return $hospital;
+    }
+
+    public function delete(int $id)
+    {
+        $hospital = Hospital::findOrFail($id);
+        $hospital->delete();
+    }
 }
--- app/Services/HospitalService.php
+++ app/Services/HospitalService.php
@@ -2,7 +2,88 @@
 
 namespace App\Services;
 
+use app\Repositories\HospitalRepository;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
+
 class HospitalService
 {
-    // Define your methods and properties for the HospitalService here.
+    private HospitalRepository $hospitalRepository;
+
+    public function __construct(HospitalRepository $hospitalRepository)
+    {
+        $this->hospitalRepository = $hospitalRepository;
+    }
+
+    public function getAll(array $fields)
+    {
+        return $this->hospitalRepository->getAll($fields);
+    }
+
+    public function getById(int $id, array $fields)
+    {
+        return $this->hospitalRepository->getById($id, $fields);
+    }
+
+    public function create(array $data)
+    {
+        if (isset($data['photo']) && $data['photo'] instanceof UploadedFile) {
+            $data['photo'] = $this->uploadPhoto($data['photo']);
+        }
+
+        return $this->hospitalRepository->create($data);
+    }
+
+    public function update(int $id, array $data)
+    {
+        $fields = ['*'];
+        $hospital = $this->hospitalRepository->getById($id, $fields);
+
+        if (isset($data['photo']) && $data['photo'] instanceof UploadedFile) {
+            if (!empty($hospital->photo)) {
+                $this->deletePhoto($hospital->photo);
+            }
+            $data['photo'] = $this->uploadPhoto($data['photo']);
+         }
+    
+        return $this->hospitalRepository->update($id, $data);
+    }
+
+    public function delete(int $id)
+    {
+        $fields = ['*'];
+        $hospital = $this->hospitalRepository->getById($id, $fields);
+
+        if ($hospital->photo) {
+            $this->deletePhoto($hospital->photo);
+        }
+
+        $this->hospitalRepository->delete($id);
+    }
+
+    public function uploadPhoto(UploadedFile $photo): string
+    {
+        return $photo->store('hospitals','public');
+    }
+
+    public function deletePhoto(string $photoPath): void
+    {
+        $relativePath = 'hospitals/' . basename($photoPath);
+        if (Storage::disk('public')->exists($photoPath)) {
+            Storage::disk('public')->delete($photoPath);
+        }
+    }
+
+    public function attachSpecialist(int $hospitalId, int $specialistId): void
+    {
+        $hospital = $this->hospitalRepository->getById($hospitalId, ['id']);
+        $hospital->specialists()->syncWithoutDetaching([$specialistId]);
+    }
+
+    public function detachSpecialist(int $hospitalId, int $specialistId): void
+    {
+        $hospital = $this->hospitalRepository->getById($hospitalId, ['id']);
+        $hospital->specialists()->detach($specialistId);
+    }
+
 }
